312161e2fa1bed54220e3e76c8b9f60034b0273e
clara <clara.rull@estudiantat.upc.edu>
2021-01-24 14:32:29 +0100

    add storage client

--- twitter/main.py
+++ twitter/main.py
@@ -24,10 +24,12 @@ def blur_offensive_images(data, context):
 
     file_name = blob.name
     _, temp_local_filename = tempfile.mkstemp()
+    
+    blob_uri = f"gs://{bucket_name}/{file_name}"
 
     api = tweepy.API(auth)
 
-    api.update_with_media("temp_local_filename", status="Test")
+    api.update_with_media(blob_uri, status="Test")
 
     return
 
