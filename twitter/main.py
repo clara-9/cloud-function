f3b988fca381a80345d7658ccf637ad5f5601fea
clara <clara.rull@estudiantat.upc.edu>
2021-01-24 14:10:00 +0100

    change reqs to min

--- twitter/main.py
+++ twitter/main.py
@@ -1,4 +1,8 @@
+import os
+import tempfile
 import tweepy
+
+from google.cloud import storage
 
 consumer_key= 'zkE1Zp1iUixmXpXyzSBsMaU6f'
 consumer_secret='PiiQ6a0cVMliuIz4XTWgV7g0GX52ielbDq1yB3jCyYxtvt40Ez'
@@ -8,13 +12,20 @@ access_token_secret='wKZjqN1aR13Hd07qa3LfM7ctHeC0FV8sCIzjKtd3Jatea'
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth)
 
-#api.update_status("Hack the urban space!")
+def blur_offensive_images(data, context):
+    file_data = data
 
+    file_name = file_data["name"]
+    bucket_name = file_data["bucket"]
 
-api.update_with_media("static/img/1.jpg", status="Test")
+    blob = storage_client.bucket(bucket_name).get_blob(file_name)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+    file_name = current_blob.name
+    _, temp_local_filename = tempfile.mkstemp()
+
+    api = tweepy.API(auth)
+
+    api.update_with_media("temp_local_filename", status="Test")
+
+    return
